c5c3f46079074da3919c181564b5880fabc9874e
Kostra0ne <guillaume.amangoua@gmail.com>
2020-02-21 10:53:51 +0100

    removed client code from component  they have to DIY and put the code in Soluce files  (added .ignore Soluce rule)

--- client/src/components/form/FormArtist.jsx
+++ client/src/components/form/FormArtist.jsx
@@ -1,154 +1,22 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 // custom tools
 import LabPreview from "../LabPreview";
-import APIHandler from "../../api/APIHandler";
 // styles
 import "./../../styles/form.css";
 
-export default withRouter(function FormArtist({
-  mode = "create",
-  _id,
-  history,
-  match
-}) {
-  const selectStylesEl = useRef(null);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [isBand, setIsBand] = useState(false);
-  const [styleID, setStyleID] = useState("");
-  const [styles, setStyles] = useState(null);
 
-  useEffect(() => {
-    // use to fetch the previous database values for the artist to update
-    const initFormData = async () => {
-      const apiRes = await APIHandler.get(`/artists/${_id}`);
-      // set the function's state with the api response
-      setName(apiRes.data.artist.name);
-      setDescription(apiRes.data.artist.description);
-      apiRes.data.artist.style && setStyleID(apiRes.data.artist.style._id);
-      setIsBand(apiRes.data.artist.isBand);
-    };
+class FormArtist extends Component {
+  render() {
+    return (
+      <>
+        <h1 className="title diy">D.I.Y (FormArtist)</h1>
+        <p>Code a form to Create/Update artists.</p>
+        <LabPreview name="artistForm" isSmall />
+        <hr />
+      </>
+    );
+  }
+}
 
-    const fetchResources = async () => {
-      // this fom should provide a list of artists and labels to display in selection/options
-      const apiRes = await APIHandler.get(`/styles/`);
-      // update the function's state with the results
-      setStyles(apiRes.data.styles);
-    };
-
-    fetchResources();
-    // When in Update mode, first we need to fetch the previous artists infos from database
-    if (mode === "edit") initFormData();
-
-    return () => console.log("cleanup");
-  }, [mode, _id]);
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    // we'll need to upload an image for the artists, so let's use a form data
-    // https://developer.mozilla.org/en-US/docs/Web/API/FormData
-    const fd = {};
-    fd.name = name;
-    fd.description = description;
-    fd.isBand = isBand;
-    fd.style = styleID;
-
-    try {
-      // send the artists to the api's server
-      // form sent as post if Create mode
-      if (mode === "create") await APIHandler.post("/artists", fd);
-      // form sent as patch if Update mode
-      else await APIHandler.patch(`/artists/${match.params.id}`, fd);
-
-      // here, we access history as a destructured props (see the parameters of this component)
-      // history is accessible since we wrapped the component in the withRouter function
-      history.push("/admin/artists");
-      // done !
-    } catch (apiErr) {
-      console.error(apiErr);
-    }
-  };
-
-  return (
-    <>
-      <h1 className="title diy">D.I.Y (FormArtist)</h1>
-      <p>Code a form to Create/Update artists.</p>
-      <LabPreview name="albumForm" isSmall/>
-      <hr />
-
-      <form className="form" onSubmit={handleSubmit}>
-        <label className="label" htmlFor="name">
-          name
-        </label>
-        <input
-          className="input"
-          id="name"
-          type="text"
-          defaultValue={name}
-          onChange={e => setName(e.target.value)}
-        />
-
-        <label className="label" htmlFor="description">
-          description
-        </label>
-        <textarea
-          className="input"
-          id="description"
-          cols="30"
-          rows="30"
-          defaultValue={description}
-          onChange={e => setDescription(e.target.value)}
-        ></textarea>
-
-        <label className="label" htmlFor="style">
-          style
-        </label>
-        {Boolean(styles && styles.length) && (
-          <select
-            className="input"
-            id="style"
-            ref={selectStylesEl}
-            value={styleID}
-            onChange={e => setStyleID(e.target.value)}
-          >
-            {styles.map((s, i) => (
-              <option value={s._id} key={i}>
-                {s.name}
-              </option>
-            ))}
-          </select>
-        )}
-        <label className="label" htmlFor="isBand">
-          is band ?
-        </label>
-        <div className="row justify-start">
-          <label className="label" htmlFor="isband">
-            yes
-          </label>
-          {/* below names are mandatory for multiple input radios tied to the same state's key/value */}
-          <input
-            id="isband"
-            type="radio"
-            name="isBand"
-            checked={isBand === true}
-            onChange={e => setIsBand(true)}
-          />
-          <label className="label" htmlFor="isnotband">
-            no
-          </label>
-          <input
-            id="isnotband"
-            type="radio"
-            name="isBand"
-            checked={isBand === false}
-            onChange={e => setIsBand(false)}
-          />
-        </div>
-
-        <button className="btn">ok</button>
-      </form>
-    </>
-  );
-});
+export default withRouter(FormArtist);
--- client/src/views/Album.jsx
+++ client/src/views/Album.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
 // custom tools
-import apiHandler from "../api/APIHandler";
+// import apiHandler from "../api/APIHandler";
 import UserContext from "../auth/UserContext";
-import Comments from "../components/comment/Comments";
-import FormatDate from "../components/FormatDate";
-import Stars from "../components/star/Stars";
+// import Comments from "../components/comment/Comments";
+// import FormatDate from "../components/FormatDate";
+// import Stars from "../components/star/Stars";
 import LabPreview from "../components/LabPreview";
 // styles
 import "../styles/album.css";
@@ -13,48 +12,8 @@ import "../styles/comment.css";
 import "../styles/star.css";
 
 export default function Album({ match }) {
-  const [album, setAlbum] = useState({});
   const userContext = useContext(UserContext);
   const { currentUser } = userContext;
-  const [userRate, setUserRate] = useState(undefined);
-
-  const getUserRate = async () => {
-    console.log(currentUser);
-
-    const apiRes = await apiHandler.get(
-      `/rates/albums/${match.params.id}/users/${currentUser._id}`
-    );
-      console.log(">>>>", apiRes);
-      
-    setUserRate(apiRes.data.userRate);
-  };
-
-  const getAlbum = async () => {
-    const apiRes = await apiHandler.get(`/albums/${match.params.id}`);
-    setAlbum(apiRes.data.album);
-  };
-
-  const updateUserRate = async evt => {
-    try {
-      const rate = Number(evt.currentTarget.getAttribute("data-rate"));
-      await apiHandler.patch(`/rates/albums/${match.params.id}`, { rate });
-      console.log("here");
-      
-      getAlbum();
-      getUserRate();
-    } catch (apiErr) {
-      console.error(apiErr);
-    }
-  };
-
-  useEffect(() => {
-    try {
-      getAlbum();
-      if (currentUser._id) getUserRate();
-    } catch (err) {
-      console.error(err);
-    }
-  }, [currentUser]);
 
   return (
     <>
@@ -62,7 +21,8 @@ export default function Album({ match }) {
       <p>
         Use the image below to code the {`<Album />`} component.
         <br />
-        This component import child components: {`<Stars />`} and {`<Comments />`}{" "}
+        This component import child components: {`<Stars />`} and{" "}
+        {`<Comments />`}{" "}
       </p>
 
       <h1 className="title diy">D.I.Y (Stars)</h1>
@@ -71,7 +31,8 @@ export default function Album({ match }) {
         <br />
         The black stars represent the average rate for a given resource.
         <br />
-        The yellow stars represent the logged in user rate fro the current album.
+        The yellow stars represent the logged in user rate fro the current
+        album.
         <br />
         Bonus: make it modular to rate labels/styles as well.
       </p>
@@ -85,49 +46,7 @@ export default function Album({ match }) {
         <br />
       </p>
 
-      <LabPreview name="album"/>
-
-      <div className="page album">
-        <h1 className="title">{album.title}</h1>
-
-        <div className="all-stars">
-          <Stars avgRate={album.avg} title="average rate" />
-          <Stars
-            css="user"
-            avgRate={userRate}
-            clbk={updateUserRate}
-            title="your rate"
-          />
-        </div>
-
-        <img
-          src={album.cover}
-          alt={`cover ${album.title} `}
-          className="cover"
-        />
-
-        <p className="description">
-          {album.description || "no description yet"}{" "}
-        </p>
-
-        <Comments type="album" id={match.params.id} />
-
-        <p className="publishing">
-          Album by&nbsp;
-          {album.artist && (
-            <Link className="link" to={`/artists/${album.artist._id}`}>
-              {album.artist.name}
-            </Link>
-          )}
-          {!album.artist && <span>unknown</span>}
-          {album.releaseDate && (
-            <span>
-              &nbsp;published the <FormatDate date={album.releaseDate} />
-            </span>
-          )}
-          <b>{album.label && <span>&nbsp;by {album.label.name}</span>}</b>
-        </p>
-      </div>
+      <LabPreview name="album" />
     </>
   );
 }
