757644348e90464bd33bac0ca992dc7ab723a4c2
KostraOne <guillaume.amangoua@gmail.com>
2020-02-24 13:00:44 +0100

    update cards for students

--- client/src/components/card/CardAlbum.jsx
+++ client/src/components/card/CardAlbum.jsx
@@ -6,14 +6,5 @@ import IconFav from "../icon/IconFavorite";
 import "./../../styles/icon-color.css";
 
 export default function CardArtist({ data }) {
-  
-  return (
-    <React.Fragment>
-      <IconFav resourceId={data._id} resourceType="albums" isAlreadyFavorite={data.isFavorite} />
-      <Link to={`/albums/${data._id}`}>
-        <h3 className="title small">{data.title}</h3>
-        <img className="cover" src={data.cover} alt="cover" />
-      </Link>
-    </React.Fragment>
-  );
+  return <div>@todo</div>;
 }
--- client/src/components/card/CardArtist.jsx
+++ client/src/components/card/CardArtist.jsx
@@ -6,23 +6,5 @@ import IconFav from "../icon/IconFavorite";
 import "./../../styles/icon-color.css";
 
 export default function CardArtist({ data }) {
-  return (
-    <>
-      <IconFav
-        isAlreadyFavorite={data.isFavorite}
-        resourceId={data._id}
-        resourceType="artists"
-      />
-
-      <Link to={`/artists/${data._id}`} className="link">
-        {data.style && data.style.color && (
-          <div
-            className="style color"
-            style={{ background: data.style.color }}
-          ></div>
-        )}
-        <h3 className="title small">{data.name}</h3>
-      </Link>
-    </>
-  );
+  return <div>@todo</div>;
 }
