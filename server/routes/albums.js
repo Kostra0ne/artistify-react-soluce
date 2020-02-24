4606269c5ed919b15d95486298174de9b89c2cca
KostraOne <guillaume.amangoua@gmail.com>
2020-02-24 12:45:33 +0100

    correct code removal that caused default app to crash

--- server/routes/albums.js
+++ server/routes/albums.js
@@ -4,7 +4,7 @@
 
 const express = require("express");
 const router = new express.Router();
-// const albumModel = require("../models/Album");
+const albumModel = require("../models/Album");
 const uploader = require("./../config/cloudinary");
 
 const getAverageRate = async idAlbum => {
@@ -14,7 +14,42 @@ const getAverageRate = async idAlbum => {
 };
 
 router.get("/albums", (req, res, next) => {
-  res.status(200).json({ msg: "@todo" })
+  // let's determine the sort query either a number or an empty object
+  const sortQ = req.query.sort
+    ? { [req.query.sort]: Number(req.query.order) }
+    : {};
+  // let's do the same with the limit query object,
+  const limitQ = req.query.limit ? Number(req.query.limit) : 10;
+
+  albumModel
+    .find() // fetch all documents from albums collection
+    .populate({
+      // populate "joins" uses provided objectId references an object from an other collection
+      path: "artist", // here the associated artist document will be fetched as well
+      populate: {
+        // one can nest population
+        path: "style" // here the style document asssociated to the artist is feched as well
+      }
+    })
+    .populate("label") // chaining population is also possible, here for label documents
+    .sort(sortQ) // the provided sort query comes into action here
+    .limit(limitQ) // same thing for the limit query
+    .then(async albums => {
+      // AVG : things are getting tricky here ! :) 
+      // the following map is async, updating each artist with an avg rate
+      const albumsWithRatesAVG = await Promise.all(
+        albums.map(async album => {
+          const copy = album.toJSON();
+          // copy.avg = await getAverageRate(album._id);
+          copy.isFavorite =
+            req.user && req.user.favorites.albums.includes(copy._id.toString());
+          return copy;
+        })
+      );
+
+      res.json({ albums: albumsWithRatesAVG });
+    })
+    .catch(next);
 });
 
 router.get("/albums/:id", (req, res, next) => {
